87f8dc988687b7b80e73f16093634f6504aa1ee8
shrikanthbolas <krshrikanth@gmail.com>
2023-05-10 19:02:47 +0545

    login test class comment the code

--- SampleProjectGITHUB/src/test/java/com/testcases/LoginTest.java
+++ SampleProjectGITHUB/src/test/java/com/testcases/LoginTest.java
@@ -4,6 +4,7 @@ public class LoginTest {
 
 	public static void main(String[] args) {
 		System.out.println("this is login test class");
+		System.out.println("this is login test class comment the code");
 	}
 
 }
