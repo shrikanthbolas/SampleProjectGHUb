9094ee15f2c73f475e32fe0c7e4b18daa3fc28dd
shrikanthbolas <krshrikanth@gmail.com>
2023-05-10 18:38:58 +0545

    my jamaica

--- SampleProjectGITHUB/src/main/java/com/pages/LoginPage.java
+++ SampleProjectGITHUB/src/main/java/com/pages/LoginPage.java
@@ -5,6 +5,7 @@ public class LoginPage {
 	public static void main(String[] args) {
 		System.out.println("this is login page");
 		System.out.println("this is new comment in page");
+		System.out.println("this is new comment implementing");
 	}
 
 }
