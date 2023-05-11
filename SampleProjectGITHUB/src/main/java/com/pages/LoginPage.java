21cc6229a367acf2395eefe6994be7aa423eb7eb
shrikanthbolas <krshrikanth@gmail.com>
2023-05-11 21:19:59 +0545

    Checked in date - 11-05-2023

--- SampleProjectGITHUB/src/main/java/com/pages/LoginPage.java
+++ SampleProjectGITHUB/src/main/java/com/pages/LoginPage.java
@@ -4,9 +4,6 @@ public class LoginPage {
 
 	public static void main(String[] args) {
 		System.out.println("this is login page");
-		System.out.println("this is new comment in page");
-		System.out.println("this is new comment implementing");
-		System.out.println("this is new comment implementing");
 		System.out.println("this new comment is not implementing now");
 	}
 
--- SampleProjectGITHUB/src/test/java/com/testcases/LoginTest.java
+++ SampleProjectGITHUB/src/test/java/com/testcases/LoginTest.java
@@ -4,7 +4,6 @@ public class LoginTest {
 
 	public static void main(String[] args) {
 		System.out.println("this is login test class");
-		System.out.println("this is login test class comment the code");
+		System.out.println("this is login test class today is thursday night checked in ----");
 	}
-
 }
