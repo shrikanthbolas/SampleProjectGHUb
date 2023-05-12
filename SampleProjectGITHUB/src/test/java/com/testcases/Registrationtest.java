525a45240bd6d4b098571fc56789c472cfa9262e
shrikanthbolas <krshrikanth@gmail.com>
2023-05-12 19:55:09 +0545

    commiting from gitbash

---
+++ SampleProjectGITHUB/src/test/java/com/testcases/Registrationtest.java
@@ -0,0 +1,13 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+public class Registrationtest {
+
+	@Test
+	public void testLogin() {
+		System.out.println("SHRI narayana");
+		System.out.println("registration class");
+	}
+
+}
